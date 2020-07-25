9885589410df3e6135158b57649a7e3703b33a47
BogdanCrs <crusoveanubogdan4@gmail.com>
2020-07-25 16:13:11 +0300

    Used styled-components library to write CSS in JS in custom-button, header and homepage to see how it works

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.component.scss';
+import { HomePageContainer } from './hompage.styles';
 
 const HomePage = () => (
-  <div className="homepage">
-   <Directory />
-  </div>
+  <HomePageContainer>
+    <Directory />
+  </HomePageContainer>
 );
 
 export default HomePage;
